0c16b08e73dc0f8338542009eb9759ce6b7d8e06
Hamza Sayed <swiiftblade96@gmail.com>
2026-04-23 15:08:23 +0200

    snapshotss

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,10 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") version "4.4.1"
 }
-dependencies {
-    classpath 'com.google.gms:google-services:4.4.1'
-}
+
 android {
     namespace = "com.example.gym_app"
     compileSdk = flutter.compileSdkVersion
@@ -22,10 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.gym_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -34,8 +29,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -44,4 +37,3 @@ android {
 flutter {
     source = "../.."
 }
-apply plugin: 'com.google.gms.google-services'
