ac3120448c77beeda81abb80ccabbfca7350bfe8
Hamza Sayed <swiiftblade96@gmail.com>
2026-04-23 13:05:37 +0200

    snapshot3

---
+++ lib/screens/HomeScreen.dart
@@ -0,0 +1,13 @@
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home")),
+      body: const Center(
+        child: Text("Welcome to SORROW FITNESS 💪"),
+      ),
+    );
+  }
+}
---
+++ lib/screens/SplashScreen.dart
@@ -0,0 +1,19 @@
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, '/login');
+    });
+
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'SORROW FITNESS',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
